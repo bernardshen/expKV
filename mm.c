92583dea51f5c3cdc72c19be077cc34a9e27fb06
bernard <jcshen@cse.cuhk.edu.hk>
2021-04-04 01:08:07 +0800

    framework

---
+++ mm.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include "mm.h"
+#include "base_table.h"
--- mm.h
+++ mm.h
@@ -1,6 +1,35 @@
 #ifndef MM_H
 #define MM_H
 
+#include <stdlib.h>
+#include "base_table.h"
 
+typedef struct _MemoryManager {
+    void * tableAddr;
+    void * itemPool;   // a pool of item;
+    void * dataAddr;   // a pointer to the storage that stores key and value can be either dataAddr1 or dataAddr2
+
+    // use two backup space for compaction
+    void * dataAddr1;  // a pointer to the storage that stores KV can be used as backup
+    void * dataAddr2;  // a pointer to the storage that stores KV can be used as backup
+
+    // allocate new item
+    void * freeItemList;
+    void * usedItemList;
+    size_t freeSize;
+    size_t usedSize;
+
+    // record the size of items and table
+    size_t tblSize;
+    size_t itemSize;
+    TableType ttype;
+} MemoryManager;
+
+
+int itemAddr2Index(MemoryManager * mm, void * addr);
+
+int initMM(MemoryManager * mm, TableType type);
+void * MMAllocItem();
+void MMFreeitem(void * itemAddr);
 
 #endif
